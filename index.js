728837d4c538676097e95aad0c1b4cf9fd363612
Alex Sabau <alex@asab-se.de>
2018-05-07 22:14:00 +0200

    Add project structure

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alex on 07.05.2018
+ * mailto:alex@asab-se.de
+ */
+'use strict';
+
+/**
+ * The entry point.
+ *
+ * @module Yamanager
+ */
+module.exports = require('./lib/yamanager');
